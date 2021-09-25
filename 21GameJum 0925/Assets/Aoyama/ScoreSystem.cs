5d76546f4a6190d9631c4e530e31d9619dcbcd32
aoyama_saito <saitomarimo1229@gmail.com>
2021-09-25 11:58:54 +0900

    ScoreManagerが完成

---
+++ 21GameJum 0925/Assets/Aoyama/ScoreSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreSystem : MonoBehaviour
+{
+    [SerializeField] Text scoreText;
+    GameObject player;
+    Transform orizinPosition;
+    public float score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.fontSize = 140;
+        player = GameObject.FindWithTag("Player");
+        orizinPosition = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = player.transform.position.x - orizinPosition.position.x;
+        scoreText.text = "score";
+    }
+}
