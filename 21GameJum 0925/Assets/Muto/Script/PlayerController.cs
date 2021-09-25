fed5a605b4a3f3b03fb7f5f8b97b0a81fdd60eb5
MutoAyumu <mutoayum0610@gmail.com>
2021-09-25 14:46:27 +0900

    配置するパーツの作成

--- 21GameJum 0925/Assets/Muto/Script/PlayerController.cs
+++ 21GameJum 0925/Assets/Muto/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(BoxCollider2D))]
@@ -9,9 +10,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float m_moveSpeed = 1f;
     [SerializeField] float m_jumpSpeed = 0f;
     [SerializeField] string m_groundTag = " ";
+    [SerializeField] string m_damageTag = " ";
+    [SerializeField] UnityEvent m_event = default;
 
     bool isJump;
 
+    Vector2 m_vectorH = Vector2.right;
+    Vector2 m_vectorV = Vector2.up;
     Rigidbody2D m_rb;
 
     private void Start()
@@ -25,21 +30,27 @@ public class PlayerController : MonoBehaviour
     }
     void MoveUpDate()
     {
-        m_rb.AddForce(this.transform.right * m_moveSpeed, ForceMode2D.Force);
+        m_rb.AddForce(m_vectorH * m_moveSpeed, ForceMode2D.Force);
     }
     public void InputJump()
     {
         if (!isJump)
         {
-            m_rb.AddForce(this.transform.up * m_jumpSpeed, ForceMode2D.Impulse);
+            m_rb.AddForce(m_vectorV * m_jumpSpeed, ForceMode2D.Impulse);
             isJump = true;
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == m_groundTag)
+        if (collision.gameObject.tag == m_groundTag)
         {
             isJump = false;
+        }
+
+        if (collision.gameObject.tag == m_damageTag)
+        {
+            m_event.Invoke();
         }
     }
 }
